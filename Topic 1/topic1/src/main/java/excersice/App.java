adae708815da06cb3929eda12a311df608f4a050
GLOBANT\jesica.restiffo <jesica.restiffo@globant.com>
2016-10-25 09:12:25 -0300

    Topic 1 - first version

---
+++ Topic 1/topic1/src/main/java/excersice/App.java
@@ -0,0 +1,13 @@
+package excersice;
+
+/**
+ * Hello Globant!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Globant!" );
+    }
+}
